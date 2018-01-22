de40c5c80feef2e378f4966b4ac8d2bb4dab5fa2
Soerine <5123688@rts-365.dk>
2018-01-22 09:09:51 +0100

    bum

---
+++ løkker.js
@@ -0,0 +1,31 @@
+let i=1;
+
+for(i; i<26; i++){
+    console.log(i)
+}
+
+let limit=26;
+let p=0;
+let q=0;
+let text= "";
+
+while(p<limit){
+    p++;
+    console.log("number " + (limit - p));
+}
+do{
+    q++;
+    console.log("nummer " + (limit - q));
+}while (q < limit);
+
+let y=2017;
+for(y; y>1916; y--){
+    console.log(y)
+}
+
+let days= Array("mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag", "søndag");
+for(let t=0; t <days.length; t++){
+    console.log(days[t]);
+}
+
+    
